0dad1fc629b7a11b02c5364730990c07b7fcdd96
Barak Chamo <barak.chamo@gmail.com>
2019-03-29 15:07:44 -0400

    build

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
 	else if(typeof define === 'function' && define.amd)
-		define([], factory);
+		define("ShaderFrame", [], factory);
 	else if(typeof exports === 'object')
 		exports["ShaderFrame"] = factory();
 	else
